7369eebb189f91908a3dc0fd217cda83723bc090
Mireille Campourcy <mireille.campourcy@gmail.com>
2017-10-06 23:49:56 +0200

    Add the last missing pictures if there is some missing

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -8,30 +8,52 @@ export default class Grid extends Component {
     constructor(props) {
         super(props);
         this.state = {posts: []};
+        this.serverRequest = '';
+        this.today = new Date();
     }
 
     componentDidMount() {
         const localPictures = localStorage.getItem('pictures');
         if(localPictures !== null) {
             const pictures = JSON.parse(localPictures);
-            this.setState({ posts: pictures.data });
+            this.setState({ posts: pictures });
         } else {
-            this.getInfo();
+            let date = new Date();
+            let tenDaysAgo = new Date(date.setDate(date.getDate() - 25));
+            this.handleConnexion(tenDaysAgo, this.today);
         }
     }
 
-    getInfo() {
-        axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2017-09-05&end_date=2017-10-02`)
-            .then(res => {
-                localStorage.setItem('pictures', JSON.stringify(res));
-            })
+    componentWillUnmount() {
+        this.serverRequest.abort();
+    }
+
+    componentDidUpdate() {
+        const lastStorageDay = new Date(this.state.posts.slice(-1)[0].date);
+        if(this.today.toLocaleDateString() !== lastStorageDay.toLocaleDateString()) {
+            const nextLastStorageDay = new Date(lastStorageDay.setDate(lastStorageDay.getDate() +1));
+            this.handleConnexion(nextLastStorageDay, this.today);
+        }
+   }
+
+    handleConnexion(firstDate, lastDate) {
+        const startDate = firstDate.toISOString().substring(0, 10);
+        const endDate = lastDate.toISOString().substring(0, 10);
+
+        this.serverRequest = axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=${startDate}&end_date=${endDate}`)
             .then(res => {
                 this.setState({ posts: res.data });
+            })
+            .then(() => {
+                localStorage.setItem('pictures', JSON.stringify(this.state.posts));
+            })
+            .catch((err) => {
+                console.log(err);
             });
     }
 
     render() {
-        const posts = this.state.posts.map((item, i) => (
+        const posts = this.state.posts.slice(0).reverse().map((item, i) => (
             <Card item={item} key={i} id={i} />
         ));
         return (
