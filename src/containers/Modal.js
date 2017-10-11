64235e6345af49f5cd6fb7378eb6f6ed86985988
Mireille Campourcy <mireille.campourcy@gmail.com>
2017-10-11 21:34:53 +0200

    Refacto of Modal

--- src/containers/Modal.js
+++ src/containers/Modal.js
@@ -1,35 +1,23 @@
-import React, { Component }  from 'react';
+import React   from 'react';
 import { X } from 'react-feather';
 import './Modal.css';
 
-export default class Modal extends Component {
+const Modal = ({item, show}) => (
+    <div className="container">
+        <div className="modal-icon">
+            <X onClick={show} />
+        </div>
+        <div className="modal-img">
+            <img src={item.url} alt={item.title}/>
+        </div>
+        <div className="modal-txt">
+            <h2>{item.title}</h2>
+            <p>{item.explanation}</p>
+            <p className='text-right'>
+                <a href={item.hdurl} target='_blank'>Télécharger l'image en HD</a>
+            </p>
+        </div>
+    </div>
+);
 
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-
-        return(
-            <div id={`modal-${this.props.id}`} ref={node => { this.node = node; }}>
-                <div className="container">
-                    <div className="modal-icon">
-                        <X onClick={this.props.onClick} />
-                    </div>
-                    <div className="modal-img">
-                        <img src={this.props.item.url} alt={this.props.item.title}/>
-                    </div>
-                    <div className="modal-txt">
-                        <h2>{this.props.item.title}</h2>
-                        <p>{this.props.item.explanation}</p>
-                        <p className='text-right'>
-                            <a href={this.props.item.hdurl} target='_blank'>Télécharger l'image en HD</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        )
-
-    }
-
-}
+export default Modal;
