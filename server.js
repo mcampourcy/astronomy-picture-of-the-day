d5147ff78ec4567565c189aff76783c4743db810
Mireille Campourcy <mireille.campourcy@gmail.com>
2017-10-14 23:15:11 +0200

    Ajout server

--- server.js
+++ server.js
@@ -2,13 +2,26 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 80;
 // load the static folder for resources
 app.use(express.static(path.join(__dirname + '/build/')));
 // run server, redirect all route on index.html for express router
 app.get('*', (req, res) => {
     // and returning the index.html file
     res.sendFile(path.join(__dirname + '/build/index.html'));
-}).listen(PORT, () => {
-    console.log(`Server on port ${PORT}`);
+}).listen(process.env.PORT, () => {
+    console.log(`Server on port ${process.env.PORT}`);
 });
+
+
+// var http = require('http');
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.write('Hello Node.js!\n');
+//     if ('host' in req.headers) {
+//         var host = req.headers['host'];
+//         res.write('Vhost is ' + host.split(':')[0] + '\n');
+//     } else {
+//         res.write('No vhost specified\n');
+//     }
+//     res.end();
+// }).listen(process.env.PORT);
