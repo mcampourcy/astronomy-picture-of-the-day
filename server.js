bf41c1d494570bf2dcd3a09dd491f3d397fa382e
Mireille Campourcy <mireille.campourcy@gmail.com>
2017-11-12 14:04:16 +0100

    Add MongoDB, Mongoose and parser

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 3000;
 // load the static folder for resources
 app.use(express.static(path.join(__dirname + '/build/')));
 // run server, redirect all route on index.html for express router
 app.get('*', (req, res) => {
     // and returning the index.html file
     res.sendFile(path.join(__dirname + '/build/index.html'));
-}).listen(process.env.PORT, () => {
-    console.log(`Server on port ${process.env.PORT}`);
+}).listen(port, () => {
+    console.log(`Server on port ${port}`);
 });
 
 
