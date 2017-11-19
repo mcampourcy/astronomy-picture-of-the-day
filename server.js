cf50ae7e85d7440b9810399ecc71bd2f50ea4ab6
Mireille Campourcy <mireille.campourcy@gmail.com>
2017-11-19 13:27:52 +0100

    Clean up

--- server.js
+++ server.js
@@ -2,27 +2,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 // load the static folder for resources
-app.use(express.static(path.join(__dirname + '/build/')));
+app.use(express.static(path.join(__dirname + '/web/compiled/')));
 // run server, redirect all route on index.html for express router
 app.get('*', (req, res) => {
     // and returning the index.html file
-    res.sendFile(path.join(__dirname + '/build/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 }).listen(port, () => {
     console.log(`Server on port ${port}`);
 });
-
-
-// var http = require('http');
-// http.createServer(function (req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     res.write('Hello Node.js!\n');
-//     if ('host' in req.headers) {
-//         var host = req.headers['host'];
-//         res.write('Vhost is ' + host.split(':')[0] + '\n');
-//     } else {
-//         res.write('No vhost specified\n');
-//     }
-//     res.end();
-// }).listen(process.env.PORT);
---
+++ src/db.js
@@ -0,0 +1,2 @@
+import 'mongoose';
+mongoose.connect('MONGO_URL=mongodb://localhost:27017/pictures');
