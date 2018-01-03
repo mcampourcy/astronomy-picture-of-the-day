340631b75237395c2f005145fa9f2ebfc18f1da3
Mireille Campourcy <mireille.campourcy@gmail.com>
2018-01-03 22:26:44 +0100

    Test sur OG scraper

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import Grid from './Grid';
-
 const ogs = require('open-graph-scraper');
-const options = {'url': 'http://ogp.me/'};
-ogs(options, function (error, results) {
-    console.log('error:', error); // This is returns true or false. True if there was a error. The error it self is inside the results object.
-    console.log('results:', results);
-});
 
 export default class App extends Component {
   render() {
+      const options = {'url': 'http://ogp.me/'};
+      ogs(options, (error, results) => {
+          console.log('error:', error); // This is returns true or false. True if there was a error. The error it self is inside the results object.
+          console.log('results:', results);
+      });
+      console.log('hey');
     return (
       <section className='container'>
           <header>
