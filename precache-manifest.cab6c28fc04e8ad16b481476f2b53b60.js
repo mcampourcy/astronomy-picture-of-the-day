ce7a122b432e50c31546b7d228f13c32e9ed4200
mcampourcy <11388201+mcampourcy@users.noreply.github.com>
2020-10-10 20:05:14 +0200

    Updates

---
+++ precache-manifest.cab6c28fc04e8ad16b481476f2b53b60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf55149d77dd90e4e4ddc6bcb549827d",
+    "url": "/astronomy-picture-of-the-day/index.html"
+  },
+  {
+    "revision": "459efc830b76a951a6fe",
+    "url": "/astronomy-picture-of-the-day/static/css/main.89164155.chunk.css"
+  },
+  {
+    "revision": "0829d8cfa07a037cedda",
+    "url": "/astronomy-picture-of-the-day/static/js/2.5dec252c.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/astronomy-picture-of-the-day/static/js/2.5dec252c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459efc830b76a951a6fe",
+    "url": "/astronomy-picture-of-the-day/static/js/main.db5e785e.chunk.js"
+  },
+  {
+    "revision": "2b4929f3982502558381",
+    "url": "/astronomy-picture-of-the-day/static/js/runtime-main.d5796d24.js"
+  }
+]);
